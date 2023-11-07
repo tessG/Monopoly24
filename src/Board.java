771605b4cf4903d6afcf07276d4253cb2abf1f1c
Anton Stengaard <afstengaard@gmail.com>
2023-11-07 12:20:15 +0100

    Board klasse. fielddata.csv er rodet og følger ikke samme regler for store/små bogstaver hele vejen igennem. så der kommer 100% fejl

--- src/Board.java
+++ src/Board.java
@@ -12,13 +12,43 @@ public class Board {
         for (int i = 0; i < fields.length; i++) {
             String[] row = data[i].split(",");
             int id = Integer.parseInt(row[0]);
-            String option = row[1];
+            String FieldType = row[1];
             String label = row[2];
             int cost = Integer.parseInt(row[3].trim());
             int income = Integer.parseInt(row[4].trim());
-            //int whatIsTessSmoking = Integer.parseInt(row[5]);
-            Field lmaoWtfIsGoingOn = new Field(id,label,cost,income);
-            fields[i] = lmaoWtfIsGoingOn;
+            int serieID = Integer.parseInt(row[5]);
+            Field f = null;
+            switch(FieldType){
+                case "Start":
+                    f = new Start(id, label, income, cost);
+                    break;
+                case "Plot":
+                    f = new Plot(id, label, income,cost,serieID);
+                    break;
+                case "Chance":
+                    f = new Chance(id, label, income,cost);
+                    break;
+                case "Tax":
+                    f = new Tax(id, label, income,cost);
+                    break;
+                case "Shipping line":
+                    f = new ShippingLine(id, label, income,cost,serieID);
+                    break;
+                case "Brewery":
+                    f = new Brewery(id, label, income,cost,serieID);
+                    break;
+                case "Parking":
+                    f = new Parking(id, label, income,cost);
+                    break;
+                case "Prison":
+                    f = new Prison(id, label, income,cost);
+                    break;
+                default: f = new Field(id,label,cost,income);
+            }
+            fields[i] = f;
+            if(f instanceof Property){
+               properties.add(f);
+            }
         }
     }
 
