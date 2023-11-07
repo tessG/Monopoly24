f9aab9f10467391d30ad72858397255edb14ea98
Marcus-Ekeroth <cph-me313@cphbusiness.dk>
2023-11-07 12:26:19 +0100

    board sprint6

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 public class Board {
     private Field[] fields;
     private String[] data;
+
+    private ArrayList<Field> properties = new ArrayList<>();
+
 
     public Board(String[] data){
         this.data = data;
@@ -17,8 +22,42 @@ public class Board {
             int cost = Integer.parseInt(row[3].trim());
             int income = Integer.parseInt(row[4].trim());
             //int whatIsTessSmoking = Integer.parseInt(row[5]);
-            Field lmaoWtfIsGoingOn = new Field(id,label,cost,income);
-            fields[i] = lmaoWtfIsGoingOn;
+            Field f = null;
+            switch(f){
+
+                case "Start": f = new Start(ID,label,income,cost);
+                    break;
+
+                case "Prison": f = new Prison(ID,label,income,cost);
+                    break;
+
+                case "Chance": f = new Chance(ID,label,income,cost);
+                    break;
+
+                case "Brewery": f = new Brewery(ID,label,income,cost);
+                    break;
+
+                case "Plot": f = new Plot(ID,label,income,cost);
+                    break;
+
+                case "ShippingLine": f = new ShippingLine(ID,label,income,cost);
+                    break;
+
+                case "Tax": f = new Tax(ID,label,income,cost);
+                    break;
+
+                case "Parkering": f = new Parkering(ID,label,income,cost);
+                    break;
+
+                default: f = new Field(id,label,cost,income);
+
+            }
+
+            if(f instanceof Property){
+
+                properties.add(f);
+
+            }
         }
     }
 
