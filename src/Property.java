c419bc319998e17eb59dc0ef1ddc1ed359389c3f
Anton Stengaard <afstengaard@gmail.com>
2023-11-08 13:50:51 +0100

    Ny property klasse

--- src/Property.java
+++ src/Property.java
@@ -1,5 +1,7 @@
 public class Property extends Field{
     private int serieID;
+    private String option;
+    private Player owner;
 
 
 
@@ -14,11 +16,24 @@ public class Property extends Field{
 
     @Override
     public String onLand(Player p){
-    return super.onLand(p);
+    String msg =  super.onLand(p);
+    if(owner.equals(null)){
+        option = "buy";
+        msg=msg.concat("Vil du købe?(Y/N:");
+    } else {
+        option = "payRent";
+        msg=msg.concat("Du skal betale" + income + "Tasty Y for at acceptere");
+    }
+    return msg;
     }
 
     @Override
     protected String onAccept(Player p){
+        if(option.equals("buy")){
+            p.buyProperty(cost);
+        }else if(option.equals(("payRent"))){
+            p.pay(income,owner);
+        }
     return "";
     }
 
