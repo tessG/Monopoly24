0f9a62fb520752ba24a1ebb79385917c07d158a7
Malte <malte.pavon.olesen@hotmail.com>
2023-11-08 14:08:12 +0100

    Commit

--- src/Chance.java
+++ src/Chance.java
@@ -8,8 +8,28 @@ public class Chance extends Field{
 
     @Override
     public String onLand(Player p){
-        String s = p.getName() + " er landet på " + this;
+        Card card = Game.cardDeck.getNext();
+        String s = super.onLand(p);
+        this.cost = card.getIncome();
+        this.income = card.getIncome();
+        this.option = card.getEvent();
+        s += "\n Træk et kort fra bunken: "+card.getMessage();
         return s;
     }
-}
 
+    public String onAccept(Player p) {
+
+        if(option.equalsIgnoreCase("payment")){
+            p.pay(cost);
+            System.out.println("Du skal betale :"+cost+",-");
+        }
+        else if(option.equalsIgnoreCase("receive")){
+            p.recieve(cost);
+            System.out.println("Du modtager :"+cost+",-");
+        }
+        else if (option.equalsIgnoreCase("1")) {
+            p.updatePosition(40-p.getPosition());
+        }
+        return "";
+    }
+}
