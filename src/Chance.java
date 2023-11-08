2665df33c43bdcfcdb3f04508431594d26f22829
larsgroenberg <144212216+larsgroenberg@users.noreply.github.com>
2023-11-08 14:42:18 +0100

    onAccept

--- src/Chance.java
+++ src/Chance.java
@@ -8,8 +8,34 @@ public class Chance extends Field{
 
     @Override
     public String onLand(Player p){
-        String s = p.getName() + " er landet på " + this;
+        Card card = Game.cardDeck.getNext();
+        String s = super.onLand(p);
+        this.cost = card.getIncome();
+        this.income = card.getIncome();
+        this.option = card.getEvent();
+        s+= "\n Træk et kort fra bunken: "+ card.getMessage();
         return s;
     }
+    public String onAccept(Player p) {
+        if (option.equals("payment")) {
+            p.pay(cost);
+        } else if(option.equals("receive")) {
+            p.recieve(cost);
+        } else if(option.equals("1")){
+            p.updatePosition(40-p.getPosition());
+        } else if(option.equals("11")) {
+            if(p.getPosition()<11) {
+                p.updatePosition(11-p.getPosition());
+            } else {
+                p.updatePosition(40-p.getPosition()+11);
+            }
+        } else if(option.equals("40")) {
+            p.updatePosition(40-p.getPosition());
+        } else if(option.equals("collect")) {
+            p.collectFromAll(income);
+        }
+        return "";
+    }
+    //
 }
 
