233ec89970d52fe8f4d27eeccec8dcc935c875bd
moldamolda <danielherlev@hotmail.com>
2023-11-08 14:21:03 +0100

    Changes by benja

--- src/Tax.java
+++ src/Tax.java
@@ -8,7 +8,7 @@ public class Tax extends Field  {
 
     @Override
     public String onLand(Player p){
-        String s = super.onLand();
+        String s = super.onLand(p);
         s += "\n Vil du betale fast beløb på " +this.cost+ " Tast J fpr at ja \n (ellers trækker vi et beløb der svarer til 10 % af dine aktiver";
         return s;
     }
